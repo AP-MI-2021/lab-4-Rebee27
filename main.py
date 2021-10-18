a9aefe082457e173622917f46c046ea994affcbd
add tema lab2 <Rebee27>
2021-10-18 16:29:37 +0300

    add main.py

--- main.py
+++ main.py
@@ -42,26 +42,46 @@ param l : lista data de la tastatura
 param n : numarul elementelor din lista date pentru suma
 return : Suma primelor n nr pozitive
 '''
-
-'''
-def suma_n_pozitive(l):
-    n = int(input("Dati numarul de numere pentru suma"))
-    nr = 0
-    for x in l:
-        while x > 0 and nr <= n:
-            suma = suma + x
-            nr = nr + 1
-    return suma
+def suma_n_pozitive(l,n):
+   suma = 0
+   nr = 0
+   for x in l:
+       if x < 1:
+           l.remove(x)
+   for i in l:
+       if nr < n:
+           suma = suma +i
+   return suma
 
 
 def test_suma_n_pozitive():
-    assert suma_n_pozitive([1, 2, 3, 4, 5]) == 15
-    assert suma_n_pozitive([-1, -2, 3, 4, 5]) == 12
-    assert suma_n_pozitive([-1, -2, -3, -4, -5]) == 0
+    assert suma_n_pozitive([1, 2, 3, 4, 5], 5) == 15
+
+
+
 '''
+4.Să se afișeze “DA” în cazul în care toate numerele pozitive din listă sunt în ordine crescătoare și “NU” altfel
+param l : lista data
+return : "da" daca nr pozitive sunt sortate ascendent, "nu" in caz contrar
+
+'''
+def pozitive_sortate_asc(l):
+    for x in l:
+        if x < 1:
+            l.remove(x)
+    if l == l.sort():
+        return True
+    return False
+
+def test_pozitive_sortate_asc():
+    assert pozitive_sortate_asc([5, 8, 2, 3]) == False
+    assert pozitive_sortate_asc([1]) == False
 
 def main():
     test_eliminare_duplicate()
+    test_suma_n_pozitive()
+    test_pozitive_sortate_asc()
+    n = int(input("Dati nr de numere pentru suma"))
     l = []
     while True:
         printMeniu()
@@ -71,9 +91,15 @@ def main():
         elif optiune == "2":
             print(eliminare_duplicate(l))
         elif optiune == "3":
-            print()
+            if suma_n_pozitive(l,n) == None:
+                print("“Dimensiunea listei este prea mică")
+            else:
+                print(suma_n_pozitive(l,n))
         elif optiune == "4":
-            print()
+            if pozitive_sortate_asc(l):
+                print("DA")
+            else:
+                print("NU")
         elif optiune == "5":
             print()
         elif optiune == "x":
