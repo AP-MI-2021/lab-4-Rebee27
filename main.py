d045d1896121d0d2b0fa20383c37e2de5f9ceeec
add tema lab2 <Rebee27>
2021-10-18 15:51:33 +0300

    add main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ def printMeniu():
     print("1.Citire lista")
     print("2.Afișarea listei după eliminarea duplicatelor")
     print("3.Afișarea sumei primelor n numere pozitive din listă, unde n se citește de la tastatură.")
-    print("4.Să se afișeze “DA” în cazul în care toate numerele pozitive din listă sunt în ordine crescătoare și “NU” altfel.")
-    print("5.Afișarea listei obținute din lista inițială în care numerele care apar doar o singură dată sunt înlocuite cu numărul de divizori proprii ai numărului.")
+    print(
+        "4.Să se afișeze “DA” în cazul în care toate numerele pozitive din listă sunt în ordine crescătoare și “NU” altfel.")
+    print(
+        "5.Afișarea listei obținute din lista inițială în care numerele care apar doar o singură dată sunt înlocuite cu numărul de divizori proprii ai numărului.")
     print("x.Iesire")
 
 
@@ -15,9 +17,51 @@ def citireLista():
         l.append(int(x))
     return l
 
+'''
+2.Afișarea listei după eliminarea duplicatelor
+param l : lista data de la tastatura
+return : lista fara duplicate
+'''
+def eliminare_duplicate(l):
+    rezultat = []
+    for i in l:
+        if i not in rezultat:
+            rezultat.append(i)
+    return rezultat
+
+
+def test_eliminare_duplicate():
+    assert eliminare_duplicate([5, 6, 6, 5, 7, 0, 6]) == [5, 6, 7, 0]
+    assert eliminare_duplicate([2, 3, 4, 5, 6]) == [2, 3, 4, 5, 6]
+    assert eliminare_duplicate([1, 1]) == [1]
+
+
+'''
+3.Afișarea sumei primelor n numere pozitive din listă, unde n se citește de la tastatură
+param l : lista data de la tastatura
+param n : numarul elementelor din lista date pentru suma
+return : Suma primelor n nr pozitive
+'''
+
+'''
+def suma_n_pozitive(l):
+    n = int(input("Dati numarul de numere pentru suma"))
+    nr = 0
+    for x in l:
+        while x > 0 and nr <= n:
+            suma = suma + x
+            nr = nr + 1
+    return suma
+
+
+def test_suma_n_pozitive():
+    assert suma_n_pozitive([1, 2, 3, 4, 5]) == 15
+    assert suma_n_pozitive([-1, -2, 3, 4, 5]) == 12
+    assert suma_n_pozitive([-1, -2, -3, -4, -5]) == 0
+'''
 
 def main():
-    '''apelare teste'''
+    test_eliminare_duplicate()
     l = []
     while True:
         printMeniu()
@@ -25,7 +69,7 @@ def main():
         if optiune == "1":
             l = citireLista()
         elif optiune == "2":
-            print()
+            print(eliminare_duplicate(l))
         elif optiune == "3":
             print()
         elif optiune == "4":
